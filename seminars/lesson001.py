c6137271fd8388441f310d171a49cd59e13b709f
Perchik <saikalrustamovna@gmail.com>
2024-03-24 00:11:57 +0600

    seminars:lesson001:task2

--- seminars/lesson001.py
+++ seminars/lesson001.py
@@ -16,4 +16,17 @@
 # print(f"Нужно {day} дн.")
 
 
-
+# 2-ОЕ ЗАДАНИЕ
+# В некоторой школе решили набрать три новых
+# математических класса и оборудовать кабинеты для
+# них новыми партами. За каждой партой может сидеть
+# два учащихся. Известно количество учащихся в
+# каждом из трех классов. Выведите наименьшее
+# число парт, которое нужно приобрести для них.
+# Input: 20 21 22(ввод чисел НЕ в одну строку)
+# Output: 32
+class1=int(input("How many people in the first class? "))
+class2=int(input("How many people in the second class? "))
+class3=int(input("How many people in the third class? "))
+desks=(class1//2+class1%2)+(class2//2+class2%2)+(class3//2+class3%2)
+print(f"The minimum desks you need to buy is {desks}")
