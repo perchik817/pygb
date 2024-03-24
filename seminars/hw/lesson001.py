a7535036c86ef0d41c023bfbc1d1d69fbc207e40
Perchik <saikalrustamovna@gmail.com>
2024-03-24 16:38:44 +0600

    seminars:hw:lesson001:task1 with str

--- seminars/hw/lesson001.py
+++ seminars/hw/lesson001.py
@@ -4,12 +4,15 @@
 # n = 123 -> res = 6 (1 + 2 + 3)
 # n = 100 -> res = 1 (1 + 0 + 0)
 
-# n = 123
+n = 123
 # first_digit = n//100
 # second_digit=(n//10)%10
 # third_digit=n%10
 # res =first_digit+second_digit+third_digit
 # print(f"n = {n} -> res = {res} ({first_digit} + {second_digit} + {third_digit})")
+n=str(n)
+res = int(n[0]) + int(n[1]) + int(n[2])
+print(res)
 
 
 
