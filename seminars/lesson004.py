b9c17d06f7f11b137c0a30ed86232e86d38b6ed9
Perchik <saikalrustamovna@gmail.com>
2024-05-12 00:06:56 +0600

    seminars:lesson004:task2

--- seminars/lesson004.py
+++ seminars/lesson004.py
@@ -1,18 +1,32 @@
+# Пользователь вводит текст(строка). Словом считается последовательность непробельных символов идущих подряд,
+# слова разделены одним или большим числом пробелов. Определите, сколько различных слов содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
+# Output: 13
+
+# user_str = input("Enter your text: ").lower().split()
+user_str = """She sells sea shells on the sea shore The shells
+that she sells are sea shells I'm sure.So if she sells sea
+shells on the sea shore I'm sure that the shells are sea
+shore shells""".lower().split()
+print(len(set(user_str)))
+
+
+
 # Напишите программу, которая принимает на вход строку, и отслеживает, сколько раз каждый символ
 # уже встречался. Количество повторов добавляется к символам с помощью постфикса формата _n.
 # Input: a a a b c a a d c d d
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 # Для решения данной задачи используйте функцию .split()
 
-string = input("enter satze: ").split()
-d = dict()
-result = list()
-for i in string:
-    if i in d.keys():
-        result.append(f"{i}_{d[i]}")
-        d[i] += 1
-    else:
-        result.append(i)
-        d[i] = 1
-print(*result)
-print(d)
+# string = input("enter satze: ").split()
+# d = dict()
+# result = list()
+# for i in string:
+#     if i in d.keys():
+#         result.append(f"{i}_{d[i]}")
+#         d[i] += 1
+#     else:
+#         result.append(i)
+#         d[i] = 1
+# print(*result)
+# print(d)
