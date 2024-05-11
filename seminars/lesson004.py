202b9974059e9824237c8ce69b41b0ac241bdcfc
Perchik <saikalrustamovna@gmail.com>
2024-05-12 01:01:13 +0600

    seminars:lesson004:task3

--- seminars/lesson004.py
+++ seminars/lesson004.py
@@ -1,15 +1,36 @@
+# Ваня и Петя поспорили, кто быстрее решит следующую задачу: “Задана последовательность неотрицательных целых чисел. Требуется определить
+# значение наибольшего элемента последовательности, которая завершается первым встретившимся нулем (число 0 не входит в
+# последовательность)”. Однако 2 друга оказались не такими смышлеными. Никто из ребят не смог до конца сделать это задание. Они решили
+# так: у кого будет меньше ошибок в коде, тот и выиграл спор. За помощью товарищи обратились к Вам, студентам.
+nums = list()
+while True:
+    num = int(input("enter a number: "))
+    if num == 0:
+        break
+    else:
+        nums.append(num)
+print(max(nums))
+# max_num = 1
+# while True:
+#     num = int(input("Enter a number: "))
+#     if num == 0:
+#         break
+#     if num > max_num:
+#         max_num = num
+# print(max_num)
+
+
 # Пользователь вводит текст(строка). Словом считается последовательность непробельных символов идущих подряд,
 # слова разделены одним или большим числом пробелов. Определите, сколько различных слов содержится в этом тексте.
 # Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
 # Output: 13
 
 # user_str = input("Enter your text: ").lower().split()
-user_str = """She sells sea shells on the sea shore The shells
-that she sells are sea shells I'm sure.So if she sells sea
-shells on the sea shore I'm sure that the shells are sea
-shore shells""".lower().split()
-print(len(set(user_str)))
-
+# user_str = """She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells""".lower().split()
+# print(len(set(user_str)))
 
 
 # Напишите программу, которая принимает на вход строку, и отслеживает, сколько раз каждый символ
