77be62c5e7109d9a2318b28440aa59858dba674a
Perchik <saikalrustamovna@gmail.com>
2024-05-11 21:43:06 +0600

    seminars:lesson003:task3

--- seminars/lesson003.py
+++ seminars/lesson003.py
@@ -12,16 +12,40 @@
 # (сдвиг - циклический) на K элементов вправо, K положительное число.
 # Input: [1, 2, 3, 4, 5] k = 3
 # Output: [4, 5, 1, 2, 3]
-l = [1, 2, 3, 4, 5]
-k = 3
+# l = [1, 2, 3, 4, 5]
+# k = 3
 
-def shift_list(l1, k):
-    size = len(l1)
-    list_l = list()
-    for i in range(size):
-        ind = (i + k) % size
-        list_l.append(l1[ind])
-    return list_l
+# def shift_list(l1, k):
+#     size = len(l1)
+#     list_l = list()
+#     for i in range(size):
+#         ind = (i + k) % size
+#         list_l.append(l1[ind])
+#     return list_l
 
-shifted_list_k = shift_list(l, k)
-print(shifted_list_k)
+# shifted_list_k = shift_list(l, k)
+# print(shifted_list_k)
+
+
+# Напишите программу для печати всех уникальных значений в словаре.
+# Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}]
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+def get_unique_values(list_of_dicts):
+    unique_values = set()
+    for d in list_of_dicts:
+        for value in d.values():
+            unique_values.add(value.strip())  # Removing leading and trailing whitespace
+    return unique_values
+
+list_of_dicts = [
+    {"V": "S001"},
+    {"V": "S002"},
+    {"VI": "S001"},
+    {"VI": "S005"},
+    {"VII": " S005 "},
+    {" V ": " S009 "},
+    {" VIII ": " S007 "},
+]
+
+unique_values = get_unique_values(list_of_dicts)
+print(unique_values)
