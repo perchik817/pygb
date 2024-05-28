0eaf197524715c5705b8eef681506735befa39d3
Perchik <saikalrustamovna@gmail.com>
2024-05-28 18:50:51 +0600

    seminars:lesson5:task2

--- seminars/lesson005.py
+++ seminars/lesson005.py
@@ -2,10 +2,23 @@
 # Требуется найти N-е число Фибоначчи
 # Input: 7
 # Output: 13
-def fib(n):
-    if n <= 1 and n >= (-1):
-        return n
-    if n < 0:
-        return (-1) * (fib(n + 1) - fib(n + 2))
-    return fib(n - 1) + fib(n - 2)
-print(fib(-2))
+# def fib(n):
+#     if n <= 1 and n >= (-1):
+#         return n
+#     if n < 0:
+#         return (-1) * (fib(n + 1) - fib(n + 2))
+#     return fib(n - 1) + fib(n - 2)
+# print(fib(-2))
+
+
+# Хакер Василий получил доступ к классному журналу и хочет заменить все свои минимальные оценки на максимальные.
+# Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
+# Input: 5 -> 1 3 3 3 4
+# Output: 1 3 3 3 1
+marks_str = "1 3 3 3 4".split()
+marks_list = [int(mark) for mark in marks_str]
+max_mark = max(marks_list)
+min_mark = min(marks_list)
+hacked_marks_list = [min_mark if mark == max_mark else mark for mark in marks_list]
+print(f"hacked marks: {hacked_marks_list}")
+
